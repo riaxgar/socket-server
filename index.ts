b218ec8ccec066fe9e0f91e5681da5918664ba7a
riaxgar <riaxgar@hotmail.com>
2021-06-18 19:32:02 -0500

    Seccion 4

--- index.ts
+++ index.ts
@@ -4,14 +4,14 @@ import router from './routes/router';
 import express from 'express';
 import cors from 'cors'
 
-const server = new Server();
+const server = Server.instance;
 
 //BodyParser
 server.app.use(express.urlencoded({extended: true}));
 server.app.use(express.json());
 
 //CORS
-server.app.use(cors({ origin: true, credentials: true}))
+//server.app.use(cors({ origin: true, credentials: true}))
 
 //Rutas de Servicios
 server.app.use('/', router);
