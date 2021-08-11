22153b5cc25b83a6ba18fed197649e627740851e
riaxgar <riaxgar@hotmail.com>
2021-08-11 13:09:41 -0500

    seccion 6 lista

--- routes/router.ts
+++ routes/router.ts
@@ -1,5 +1,6 @@
 import { Router, Request, Response} from 'express'
 import Server from '../classes/server';
+import { usuariosConectados } from '../sockets/socket';
 
 const router = Router();
 
@@ -44,4 +45,29 @@ router.post('/mensajes/:id', (req:Request, res: Response) => {
     });
 });
 
+//Servicio para obtener todos los Id's de los usuarios
+router.get('/usuarios', (req: Request, res: Response) => {
+    const server = Server.instance;
+    server.io.allSockets().then((clientes) => {
+        res.json({
+            ok: true,
+            clientes: Array.from(clientes)
+        });
+    }).catch((err) => {
+        res.json({
+            ok:false,
+            err
+        });
+    });        
+});
+
+//Obtener usuarios y sus nombres
+router.get('/usuarios/detalle', (req: Request, res: Response) => {
+    
+    res.json({
+        ok: true,
+        clientes: usuariosConectados.getlista()
+    });
+});
+    
 export default router;
